96a8492878ef2129625b4fface483b0dc4611932
coder-mansi-thakur <mansi.thakur@pasarpolis.co.id>
2023-08-22 15:14:30 +0530

    main code shifted to contributors page

---
+++ app/contributors/page.tsx
@@ -0,0 +1,29 @@
+"use client"
+import { } from 'next/font/google'
+import { BasicInfo } from "@/containers/BasicIntro/BasicIntro";
+import { About } from "@/containers/About/About";
+import { TechStack } from "@/containers/TechStack";
+import { Nuggets } from "@/containers/Nuggets";
+import { Blogs } from "@/containers/Blogs";
+import { Contact } from "@/containers/Contact";
+
+import Header from '@/components/Header'
+import Footer from '@/components/Footer'
+
+// app/contributors/mansi-thakur/pages.tsx
+
+export default function Page() {
+
+  return (
+    <>
+     <h1>asdnwlekfnwe</h1>
+    </>
+  );
+}
+
+
+// const App = ({ Component, pageProps }) => {
+//   return <Component {...pageProps} />;
+// };
+
+// export default App;
--- app/page.tsx
+++ app/page.tsx
@@ -1,36 +1,10 @@
-"use client"
-import { } from 'next/font/google'
-import { BasicInfo } from "@/containers/BasicIntro/BasicIntro";
-import { About } from "@/containers/About/About";
-import { TechStack } from "@/containers/TechStack";
-import { Nuggets } from "@/containers/Nuggets";
-import { Blogs } from "@/containers/Blogs";
-import { Contact } from "@/containers/Contact";
-
-import Header from '@/components/Header'
-import Footer from '@/components/Footer'
-
 
 
 export default function Page() {
 
   return (
     <>
-      <Header />
-      <BasicInfo />
-      <About />
-      <TechStack />
-      <Nuggets />
-      {false && <Blogs />}
-      <Contact />
-      <Footer />
+     <h1>hello</h1>
     </>
   );
 }
-
-
-// const App = ({ Component, pageProps }) => {
-//   return <Component {...pageProps} />;
-// };
-
-// export default App;
