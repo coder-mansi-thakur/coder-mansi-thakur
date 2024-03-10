e19d2f25531557dbbe963602da3f26c09a1e34dc
coder-mansi-thakur <mansi.thakur@pasarpolis.co.id>
2024-03-10 19:29:29 +0530

    something

--- app/contributors/page.tsx
+++ app/contributors/page.tsx
@@ -1,14 +1,14 @@
 "use client"
-import { } from 'next/font/google'
-import { BasicInfo } from "@/containers/BasicIntro/BasicIntro";
-import { About } from "@/containers/About/About";
-import { TechStack } from "@/containers/TechStack";
-import { Nuggets } from "@/containers/Nuggets";
-import { Blogs } from "@/containers/Blogs";
-import { Contact } from "@/containers/Contact";
+// import { } from 'next/font/google'
+// import { BasicInfo } from "@/containers/BasicIntro/BasicIntro";
+// import { About } from "@/containers/About/About";
+// import { TechStack } from "@/containers/TechStack";
+// import { Nuggets } from "@/containers/Nuggets";
+// import { Blogs } from "@/containers/Blogs";
+// import { Contact } from "@/containers/Contact";
 
-import Header from '@/components/Header'
-import Footer from '@/components/Footer'
+// import Header from '@/components/Header'
+// import Footer from '@/components/Footer'
 
 // app/contributors/mansi-thakur/pages.tsx
 
